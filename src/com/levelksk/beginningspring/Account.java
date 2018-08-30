28b696186b032b6c7d37cc9520c72589cb5ef603
levelksk <xulingfengcn@gmail.com>
2018-08-30 17:05:03 +0800

    Account

---
+++ src/com/levelksk/beginningspring/Account.java
@@ -0,0 +1,42 @@
+package com.levelksk.beginningspring;
+
+import java.util.Date;
+
+public class Account {
+    private long id;
+    private String ownerName;
+    private double balance;
+    private Date accessTime;
+
+    public Date getAccessTime() {
+        return accessTime;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setAccessTime(Date accessTime) {
+        this.accessTime = accessTime;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+}
