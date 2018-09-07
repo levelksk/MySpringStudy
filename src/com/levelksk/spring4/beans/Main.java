0692948258fa49ad4b03da6f07fa169385867c0e
levelksk <xulingfengcn@gmail.com>
2018-09-07 08:57:57 +0800

    spring2

--- src/com/levelksk/spring4/beans/Main.java
+++ src/com/levelksk/spring4/beans/Main.java
@@ -21,6 +21,33 @@ public class Main {
 
         helloWorld.hello();
 
+        Car car1 = (Car)ctx.getBean("car1");
+
+        System.out.println(car1);
+
+        Car car2 = (Car)ctx.getBean("car2");
+
+        System.out.println(car2);
+
+        Car car3 = (Car)ctx.getBean("car3");
+
+        System.out.println(car3);
+
+        Person person1 = (Person)ctx.getBean("person1");
+
+        System.out.println(person1);
+
+        Person person2 = (Person)ctx.getBean("person2");
+
+        System.out.println(person2);
+
+        Person person3 = (Person)ctx.getBean("person3");
+
+        System.out.println(person3);
+
+        Person person4 = (Person)ctx.getBean("person4");
+
+        System.out.println(person4);
     }
 
 }
